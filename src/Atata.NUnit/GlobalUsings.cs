9affde753b1bbf95589b9135ec646921e0df43a5
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-12-26 11:29:45 +0100

    Update `Guard`

--- src/Atata.NUnit/GlobalUsings.cs
+++ src/Atata.NUnit/GlobalUsings.cs
@@ -1,4 +1,5 @@
 ﻿global using System.Collections.Concurrent;
+global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Reflection;
 global using System.Runtime.CompilerServices;
